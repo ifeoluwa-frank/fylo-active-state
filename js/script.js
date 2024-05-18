46b3d2ecee64e487ba9eccd478dd40c3361f9c6e
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-05-18 13:32:05 +0100

    js fix

--- js/script.js
+++ js/script.js
@@ -4,14 +4,15 @@ let emailField = document.getElementById("form-input");
 let emailError = document.getElementById("email-error");
 
 function validateEmail() {
-  if (
+  if (emailField.value === "") {
+    emailError.textContent = "";
+  } else if (
     !emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
   ) {
     emailError.innerHTML = "Please check your email";
     emailField.style.borderColor = "red";
     return false;
   }
-
   emailError.innerHTML = "";
   emailField.style.borderColor = "";
   return true;
@@ -22,7 +23,9 @@ let section3EmailField = document.querySelector(".section-3-input");
 let errorMsg = document.querySelector("#email-error-section-3");
 
 function validateEmailSectionThree() {
-  if (
+  if (section3EmailField.value === "") {
+    errorMsg.textContent = "";
+  } else if (
     !section3EmailField.value.match(
       /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/
     )
